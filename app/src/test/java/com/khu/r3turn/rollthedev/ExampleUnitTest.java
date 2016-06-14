c0bad34fe8f8070c069e5be8b35ccccd043dd668
nypark <nypark425@gmail.com>
2016-06-14 12:50:20 +0900

    add android project with android studio

---
+++ app/src/test/java/com/khu/r3turn/rollthedev/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.khu.r3turn.rollthedev;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
